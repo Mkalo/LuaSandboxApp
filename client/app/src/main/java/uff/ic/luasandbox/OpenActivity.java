5fcf629712fd2ef88adafd7e982defa29036eaed
Mkalo <mkalo@xtibia.com>
2017-05-20 18:37:04 -0300

    A couple of stuff

--- client/app/src/main/java/uff/ic/luasandbox/OpenActivity.java
+++ client/app/src/main/java/uff/ic/luasandbox/OpenActivity.java
@@ -1,7 +1,10 @@
 package uff.ic.luasandbox;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class OpenActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class OpenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open);
+        Toolbar openFileToolbar = (Toolbar) findViewById(R.id.openFileToolbar);
+        setSupportActionBar(openFileToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(false);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
